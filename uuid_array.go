94f804b05236ad5cd6d581ed72281f89765397b3
Julien Maitrehenry <julien@kumojin.com>
2023-03-06 14:57:19 -0500

    Add UUIDArray struct for handling them correctly with databases (#10)
    Drop support for Go 1.15 and add support for Go 1.20
    Update golangci-lint to 1.51.2
    Change master for main

---
+++ uuid_array.go
@@ -0,0 +1,27 @@
+package uuid
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
+type UUIDArray []UUID
+
+func (a *UUIDArray) Scan(val interface{}) error {
+	switch v := val.(type) {
+	case nil:
+		*a = []UUID{}
+		return nil
+	case []byte:
+		return json.Unmarshal(v, &a)
+	case string:
+		return json.Unmarshal([]byte(v), &a)
+	default:
+		return fmt.Errorf("Unsupported type: %T", v)
+	}
+}
+
+func (a UUIDArray) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
